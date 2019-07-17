c1caf9ee1a5cdcca200d2ed0f9a30424b5f94d0e
varishtsg <varishtsg@gmail.com>
2019-07-17 21:36:56 +0530

    Added fixes for resizing particles canvas bug

--- script/script.js
+++ script/script.js
@@ -1,3 +1,14 @@
 particlesJS.load('particles-js', 'css/particlesjs-config.json', function() {
-    console.log('callback - particles.js config loaded');
+    // console.log('callback - particles.js config loaded');
   });
+
+
+// Handle window resize
+
+
+window.addEventListener('resize',function(){
+  var canvas = document.querySelector('canvas');
+  
+  canvas.height = this.window.innerHeight;
+  canvas.width = this.window.innerWidth;
+});
